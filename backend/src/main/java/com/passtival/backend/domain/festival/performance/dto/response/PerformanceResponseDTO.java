382f2232aa42019c191c1df574016d66968ba1c7
jae-2024 <dbswotkd2019@naver.com>
2025-08-10 20:59:00 +0900

    feat: 공연 ResponseDTO 추가 #24

---
+++ backend/src/main/java/com/passtival/backend/domain/festival/performance/dto/response/PerformanceResponseDTO.java
@@ -0,0 +1,36 @@
+package com.passtival.backend.domain.festival.performance.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.passtival.backend.domain.festival.performance.model.Performance;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PerformanceResponseDTO {
+
+	private final String title;
+	private final String artist;
+	private final LocalDateTime startAt;
+	private final LocalDateTime endAt;
+	private final String area;
+	private final String imagePath;
+
+	public static PerformanceResponseDTO of(Performance performance) {
+
+		if (performance == null) {
+			// performance가 아예 존재하지 않을 경우
+			throw new IllegalArgumentException("performance 정보가 존재하지 않습니다.");
+		}
+		return PerformanceResponseDTO.builder()
+			.title(performance.getTitle())
+			.artist(performance.getArtist())
+			.startAt(performance.getStartAt())
+			.endAt(performance.getEndAt())
+			.area(performance.getArea())
+			.imagePath(performance.getImagePath())
+			.build();
+	}
+}
