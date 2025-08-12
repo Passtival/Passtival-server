415a232d1102ea7a9390aa1764c7c2afb4b407ff
jae-2024 <dbswotkd2019@naver.com>
2025-08-12 10:42:50 +0900

    refactor: 오류 메시지 추가 #26

--- backend/src/main/java/com/passtival/backend/global/common/BaseResponseStatus.java
+++ backend/src/main/java/com/passtival/backend/global/common/BaseResponseStatus.java
@@ -15,6 +15,10 @@ public enum BaseResponseStatus {
 	LOGIN_REQUIRED(false, 401, "로그인이 필요합니다."),
 	ACCESS_DENIED(false, 403, "해당 리소스에 접근할 수 없습니다."),
 
+	// 추가한 오류 메시지
+	NAME_INVALID(false, 404, "해당 이름과 일치하는 데이터가 존재하지 않습니다."),
+	DATA_NULL(false, 404, "데이터가 존재하지 않습니다."),
+
 	// 500: 서버 오류
 	INTERNAL_SERVER_ERROR(false, 500, "서버에서 오류가 발생하였습니다."),
 	DATABASE_ERROR(false, 500, "데이터베이스 오류가 발생했습니다."),
