a5353a25a261adf3e251247f5c0aab507f72fa6a
jae-2024 <dbswotkd2019@naver.com>
2025-08-12 10:47:09 +0900

    feature: 부스 ResponseDTO 추가 #26

---
+++ backend/src/main/java/com/passtival/backend/domain/festival/booth/model/response/BoothResponseDTO.java
@@ -0,0 +1,47 @@
+package com.passtival.backend.domain.festival.booth.model.response;
+
+import java.time.LocalDateTime;
+
+import com.passtival.backend.domain.festival.booth.model.entity.Booth;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class BoothResponseDTO {
+
+	private final String name;
+	private final String type;
+	private final String department;
+	private final LocalDateTime operatingStart;
+	private final LocalDateTime operatingEnd;
+	private final String location;
+	private final String info;
+	private final String imagePath;
+	private final String locationImagePath;
+
+	/**
+	 * Booth 엔티티를 BoothResponseDTO로 변환하는 정적 팩토리 메서드
+	 *
+	 * @param booth Booth 엔티티
+	 * @return BoothResponseDTO 객체
+	 * @throws IllegalArgumentException booth가 null인 경우
+	 */
+	public static BoothResponseDTO of(Booth booth) {
+		if (booth == null) {
+			throw new IllegalArgumentException("Booth 값이 null입니다.");
+		}
+		return BoothResponseDTO.builder()
+			.name(booth.getName())
+			.type(booth.getType())
+			.department(booth.getDepartment())
+			.operatingStart(booth.getOperatingStart())
+			.operatingEnd(booth.getOperatingEnd())
+			.location(booth.getLocation())
+			.info(booth.getInfo())
+			.imagePath(booth.getImagePath())
+			.locationImagePath(booth.getLocationImagePath())
+			.build();
+	}
+}
