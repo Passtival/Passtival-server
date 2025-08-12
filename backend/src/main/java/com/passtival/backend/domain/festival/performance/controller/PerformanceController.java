ef2a633b3c67e2deb6714c0dada354cfe4963562
jae-2024 <dbswotkd2019@naver.com>
2025-08-12 23:17:39 +0900

    feat: 공연 이름으로 단일 조회 추가 #24

--- backend/src/main/java/com/passtival/backend/domain/festival/performance/controller/PerformanceController.java
+++ backend/src/main/java/com/passtival/backend/domain/festival/performance/controller/PerformanceController.java
@@ -4,11 +4,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.passtival.backend.domain.festival.performance.model.response.PerformanceResponseDTO;
 import com.passtival.backend.domain.festival.performance.model.entity.Performance;
+import com.passtival.backend.domain.festival.performance.repository.PerformanceRepository;
 import com.passtival.backend.domain.festival.performance.service.PerformanceService;
 import com.passtival.backend.global.common.BaseResponse;
 import com.passtival.backend.global.common.BaseResponseStatus;
@@ -28,7 +30,7 @@ public class PerformanceController {
 	 * 예: /api/performances?page=0&size=10&sort=date,desc
 	 */
 	@GetMapping("/performance")
-	public BaseResponse<?> getPerformances (
+	public BaseResponse<?> getPerformances(
 		@PageableDefault(size = 5) Pageable pageable) throws BaseException {
 		try {
 			Page<Performance> page = performanceService.getAllPerformances(pageable);
@@ -38,4 +40,15 @@ public class PerformanceController {
 			return BaseResponse.fail(BaseResponseStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
+	/**
+	 * 공연 이름으로 단일 조회
+	 */
+	@GetMapping("performance/{name}")
+	public BaseResponse<PerformanceResponseDTO> getPerformanceByName(
+		@PathVariable("performanceName") String performanceName) throws BaseException {
+		PerformanceResponseDTO performanceResponseDTO = performanceService.getPerformanceByName(performanceName);
+		return BaseResponse.success(performanceResponseDTO);
+	}
+
 }
