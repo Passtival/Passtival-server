9ae091dd84ef2fe7be6eeeb6c4135c9c342f748f
jae-2024 <dbswotkd2019@naver.com>
2025-08-14 00:56:48 +0900

    refactor: 노래엔티티와 공연엔티티 매핑 #24

--- backend/src/main/java/com/passtival/backend/domain/festival/performance/model/entity/Performance.java
+++ backend/src/main/java/com/passtival/backend/domain/festival/performance/model/entity/Performance.java
@@ -1,21 +1,30 @@
 package com.passtival.backend.domain.festival.performance.model.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.passtival.backend.global.common.model.BaseEntity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "performance")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Builder
 public class Performance extends BaseEntity {
 
@@ -45,11 +54,18 @@ public class Performance extends BaseEntity {
 	@Column(name = "performance_introduction")
 	private String introduction;
 
-	@Column(name = "performance_info")
-	private String info;
+	@Builder.Default
+	@OneToMany(mappedBy = "performance", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Song> songs = new ArrayList<>();
+
 
 	// 일차를 구분해주는 속성
 	@Column(name = "performance_day")
 	private Integer day;
+
+	public void addSong(Song song) {
+		this.songs.add(song);
+		song.setPerformance(this);
+	}
 
 }
