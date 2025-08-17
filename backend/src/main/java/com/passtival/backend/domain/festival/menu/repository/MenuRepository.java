602c2c0045c07d710a9072e2bb4ec9ddd2865a6e
jae-2024 <dbswotkd2019@naver.com>
2025-08-17 15:57:03 +0900

    remove: MenuRepository, MenuService 삭제 #35

--- backend/src/main/java/com/passtival/backend/domain/festival/menu/repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.passtival.backend.domain.festival.menu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.passtival.backend.domain.festival.menu.model.entity.Menu;
-
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-
-}
