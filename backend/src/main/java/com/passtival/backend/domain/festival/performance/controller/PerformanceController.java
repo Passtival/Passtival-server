4ce76a3240e3afd88ef5c4031f671e0dde05b09e
jae-2024 <dbswotkd2019@naver.com>
2025-08-22 14:57:21 +0900

    refactor: swagger API이름 수정 #24

--- backend/src/main/java/com/passtival/backend/domain/festival/performance/controller/PerformanceController.java
+++ backend/src/main/java/com/passtival/backend/domain/festival/performance/controller/PerformanceController.java
@@ -14,14 +14,14 @@ import com.passtival.backend.domain.festival.performance.model.response.Performa
 import com.passtival.backend.domain.festival.performance.model.entity.Performance;
 import com.passtival.backend.domain.festival.performance.service.PerformanceService;
 import com.passtival.backend.global.common.BaseResponse;
-import com.passtival.backend.global.common.BaseResponseStatus;
-import com.passtival.backend.global.exception.BaseException;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/festival")
+@Tag(name = "Performance-API", description = "공연 목록 조회 API")
 public class PerformanceController {
 
 	private final PerformanceService performanceService;
@@ -33,7 +33,7 @@ public class PerformanceController {
 	 */
 	@GetMapping("/performance")
 	public BaseResponse<?> getPerformances(
-		@PageableDefault(size = 5) Pageable pageable) throws BaseException {
+		@PageableDefault(size = 5) Pageable pageable) {
 		Page<Performance> page = performanceService.getAllPerformances(pageable);
 		Page<PerformanceResponse> dtoPage = page.map(PerformanceResponse::of);
 		return BaseResponse.success(dtoPage);
@@ -59,7 +59,7 @@ public class PerformanceController {
 	 */
 	@GetMapping("/performance/{performanceTitle}")
 	public BaseResponse<PerformanceDetailResponse> getPerformanceByTitle(
-		@PathVariable String performanceTitle) throws BaseException {
+		@PathVariable String performanceTitle) {
 		PerformanceDetailResponse detail = performanceService.getPerformanceByTitle(performanceTitle);
 		return BaseResponse.success(detail);
 	}
