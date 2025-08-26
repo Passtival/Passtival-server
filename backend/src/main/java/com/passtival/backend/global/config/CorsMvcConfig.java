d886ffc6593777efa55f0fe67e80f949c58781b5
goodjunseon <pzs2001926@gmail.com>
2025-08-26 21:39:24 +0900

    style: 메서드 위치 변경

--- backend/src/main/java/com/passtival/backend/global/config/CorsMvcConfig.java
+++ backend/src/main/java/com/passtival/backend/global/config/CorsMvcConfig.java
@@ -13,8 +13,8 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 			.allowedOrigins("https://passtival-client.vercel.app")// 실제 배포된 클라이언트 도메인
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
 			.allowedHeaders("*") // 모든 헤더 허용
-			.allowCredentials(true) // 쿠키 허용
 			.exposedHeaders("Authorization") // 클라이언트에서 접근할 수 있는 헤더
+			.allowCredentials(true) // 쿠키 허용
 			.maxAge(3600); // 1시간
 
 	}
