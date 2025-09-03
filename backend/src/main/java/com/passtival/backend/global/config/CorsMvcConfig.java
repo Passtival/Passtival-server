36756b96535e0eac427b98794c038c16384565c9
goodjunseon <pzs2001926@gmail.com>
2025-09-03 14:42:13 +0900

    fix: 배포 환경 Swagger CORS 문제 해결

--- backend/src/main/java/com/passtival/backend/global/config/CorsMvcConfig.java
+++ backend/src/main/java/com/passtival/backend/global/config/CorsMvcConfig.java
@@ -10,7 +10,11 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
-			.allowedOrigins("https://passtival.co.kr")// 실제 배포된 클라이언트 도메인
+			.allowedOrigins(
+				"https://passtival.co.kr", // 실제 배포된 클라이언트 도메인
+				"https://passtival.cloud:8080",
+				"http://passtival.cloud:8080"
+			)
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
 			.allowedHeaders("*") // 모든 헤더 허용
 			.exposedHeaders("Authorization") // 클라이언트에서 접근할 수 있는 헤더
