8b91cf9b14c0b182194cbb7b9a6be38685d67c1b
goodjunseon <pzs2001926@gmail.com>
2025-09-04 20:40:24 +0900

    feat: 응모시 회원 프로필 조회 API

--- backend/src/main/java/com/passtival/backend/domain/raffle/controller/RaffleController.java
+++ backend/src/main/java/com/passtival/backend/domain/raffle/controller/RaffleController.java
@@ -1,62 +1,41 @@
-// package com.passtival.backend.domain.raffle.controller;
-//
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-//
-// import com.passtival.backend.domain.raffle.model.request.ApplicantRegistrationRequest;
-// import com.passtival.backend.domain.raffle.service.RaffleService;
-// import com.passtival.backend.global.common.BaseResponse;
-//
-// import io.swagger.v3.oas.annotations.Operation;
-// import io.swagger.v3.oas.annotations.media.Content;
-// import io.swagger.v3.oas.annotations.media.ExampleObject;
-// import io.swagger.v3.oas.annotations.media.Schema;
-// import io.swagger.v3.oas.annotations.tags.Tag;
-// import jakarta.validation.Valid;
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.slf4j.Slf4j;
-//
-// @Slf4j
-// @RestController
-// @RequestMapping("/api/raffle")
-// @RequiredArgsConstructor
-// @Tag(name = "응모권 관련 API", description = "상품 조회, 신청자 등록")
-// public class RaffleController {
-//
-// 	private final RaffleService raffleService;
-//
-// 	@Operation(
-// 		summary = "신청자 등록",
-// 		description = "신청자의 이름과 학번, 인증키를 입력받아 신청자를 등록합니다.",
-// 		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-// 			description = "신청자 등록 요청 정보",
-// 			required = true,
-// 			content = @Content(
-// 				mediaType = "application/json",
-// 				schema = @Schema(implementation = ApplicantRegistrationRequest.class),
-// 				examples = @ExampleObject(
-// 					name = "신청자 등록 요청 예시",
-// 					value = """
-// 						{
-// 						  "applicantName": "박준선",
-// 						  "studentId": "2021U2317",
-// 						  "authenticationKey": "1234"
-// 						}
-// 						"""
-// 				)
-// 			)
-// 		)
-// 	)
-// 	@PostMapping("/applicants")
-// 	public BaseResponse<Void> saveApplicant(@Valid @RequestBody ApplicantRegistrationRequest request) {
-// 		/* 1. 클라이언트로부터 신청자의 이름과 학번, 인증키 입력받는다.
-// 		 * 2. 신청자의 이름과 학번을 기반으로 신청자를 데이터베이스에 저장한다.
-// 		 * 3. 신청이 완료되면 클라이언트에게 성공 메시지를 반환한다.
-// 		 * 4. 만약 신청자의 이름과 학번이 같은 경우, 이미 신청한 것으로 간주하고 에러 메시지를 반환한다.
-// 		 */
-// 		raffleService.registerApplicant(request);
-// 		return BaseResponse.success(null);
-// 	}
-// }
+package com.passtival.backend.domain.raffle.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.passtival.backend.domain.member.service.MemberRaffleService;
+import com.passtival.backend.domain.raffle.model.response.MemberRaffleProfileResponse;
+import com.passtival.backend.global.common.BaseResponse;
+import com.passtival.backend.global.security.model.CustomMemberDetails;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/raffle")
+@RequiredArgsConstructor
+@Tag(name = "응모권 관련 API", description = "상품 조회, 신청자 등록")
+public class RaffleController {
+
+	private final MemberRaffleService memberRaffleService;
+
+	// 회원 정보 조회
+	@GetMapping("/member")
+	@Operation(summary = "회원 응모권 정보 조회",
+		description = "로그인한 회원의 응모권 정보를 조회합니다. (응모권 개수, 응모권 사용 내역 등)",
+		security = @SecurityRequirement(name = "jwtAuth"))
+	public BaseResponse<MemberRaffleProfileResponse> getMemberRaffleProfile(
+		@AuthenticationPrincipal CustomMemberDetails member) {
+
+		MemberRaffleProfileResponse response = memberRaffleService.getMemberRaffleProfile(member.getMemberId());
+		return BaseResponse.success(response);
+
+	}
+
+}
