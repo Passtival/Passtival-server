603d5ff0f1c8b8f62c643385dea9eec77b9fc14f
jae-2024 <dbswotkd2019@naver.com>
2025-09-09 21:03:27 +0900

    fix: Performance에 불필요한 코드 제거 #128

--- backend/src/main/java/com/passtival/backend/domain/festival/performance/service/PerformanceService.java
+++ backend/src/main/java/com/passtival/backend/domain/festival/performance/service/PerformanceService.java
@@ -27,39 +27,6 @@ public class PerformanceService {
 
 	private final PerformanceRepository performanceRepository;
 
-	/**
-	 * 모든 공연 목록 조회 (페이징 가능)
-	 * @param pageable 페이지 요청 정보
-	 * 값이 비었을 때 : PERFORMANCE_NOT_FOUND에러 메시지
-	 * @return Page<Performance>
-	 */
-	public Page<Performance> getAllPerformances(Pageable pageable) {
-		Page<Performance> page = performanceRepository.findAll(pageable);
-		if (page.isEmpty()) {
-			throw new BaseException(BaseResponseStatus.PERFORMANCE_NOT_FOUND);
-		}
-		return page;
-	}
-
-	/**
-	 * 커서기반 페이지네이션
-	 */
-	public CursorPageResponse<PerformanceResponse> getPerformances(Long cursorId, int size) {
-		Pageable pageable = PageRequest.of(0, size); // offset=0 고정
-		List<Performance> performances = performanceRepository.findPageByCursor(cursorId, pageable);
-
-		if (performances.isEmpty()) {
-			throw new BaseException(BaseResponseStatus.BOOTH_NOT_FOUND); // 부스 없음 예외
-		}
-
-		Long nextCursor = performances.isEmpty() ? null : performances.get(performances.size() - 1).getId();
-
-		return new CursorPageResponse<>(
-			performances.stream().map(PerformanceResponse::of).toList(),
-			nextCursor
-		);
-	}
-
 	// 공연 이름 id로 조회
 	public PerformanceDetailResponse getPerformanceById(Long performanceId) {
 		Performance performance = performanceRepository.findById(performanceId)
