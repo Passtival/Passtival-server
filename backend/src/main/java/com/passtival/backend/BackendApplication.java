fb070d2bbe2397255bf401b4e539c7d6d841a212
goodjunseon <pzs2001926@gmail.com>
2025-09-11 14:09:26 +0900

    refactor: JPA auditing TimeZone 설정

--- backend/src/main/java/com/passtival/backend/BackendApplication.java
+++ backend/src/main/java/com/passtival/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.passtival.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
