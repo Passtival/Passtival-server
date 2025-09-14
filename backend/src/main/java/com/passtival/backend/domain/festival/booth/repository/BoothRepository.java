c69ed07a48b1864b1421d41e71244201eef5f036
goodjunseon <pzs2001926@gmail.com>
2025-09-14 17:38:40 +0900

    fix: 쿼리문 수정

--- backend/src/main/java/com/passtival/backend/domain/festival/booth/repository/BoothRepository.java
+++ backend/src/main/java/com/passtival/backend/domain/festival/booth/repository/BoothRepository.java
@@ -9,25 +9,23 @@ import org.springframework.data.repository.query.Param;
 
 import com.passtival.backend.domain.festival.booth.model.entity.Booth;
 
-
 public interface BoothRepository extends JpaRepository<Booth, Long> {
 
 	@Query("""
-	SELECT b FROM Booth b
-	WHERE (:cursorId IS NULL OR b.id < :cursorId)
-	ORDER BY
-  	CASE b.type
-  	  WHEN '학내부스' THEN 1
-  	  WHEN '체험'   THEN 2
- 	   WHEN '푸드존' THEN 3
-   	 WHEN '의료지원' THEN 4
-  	  ELSE 5
-  	END,
-  	b.name ASC,
-  	b.id DESC
-	""")
-	List<Booth> findPageByCursor(@Param("cursorId") Long cursorId,
-		Pageable pageable);
+		SELECT b FROM Booth b
+		WHERE (:cursorId IS NULL OR b.id < :cursorId)
+		ORDER BY
+		    CASE b.type
+		        WHEN '학내부스' THEN 1
+		        WHEN '체험' THEN 2
+		        WHEN '푸드존' THEN 3
+		        WHEN '의료지원' THEN 4
+		        ELSE 5
+		    END,
+		    b.name ASC,
+		    b.id DESC
+		""")
+	List<Booth> findPageByCursor(@Param("cursorId") Long cursorId, Pageable pageable);
 
 	boolean existsByName(String name);
 
