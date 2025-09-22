eebbe9a444624b2e77d2cf944964be2ce372e824
goodjunseon <pzs2001926@gmail.com>
2025-09-22 11:24:37 +0900

    fix: booth_location_id 수정

--- backend/src/main/java/com/passtival/backend/domain/festival/booth/model/entity/Booth.java
+++ backend/src/main/java/com/passtival/backend/domain/festival/booth/model/entity/Booth.java
@@ -36,7 +36,7 @@ public class Booth extends BaseEntity {
 	@Column(name = "booth_id")
 	private Long id;
 
-	@Column(name = "location_id")
+	@Column(name = "booth_location_id")
 	private Long location_id;
 
 	@Column(name = "booth_name")
